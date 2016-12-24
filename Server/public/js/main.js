dbf6ecb4df7cd2f01a92cbd5fe3d4c9d7b884085
Edward Digby <edward@ejdigby.com>
2016-12-24 20:37:59 +0000

    Finished add and view for server

--- Server/public/js/main.js
+++ Server/public/js/main.js
@@ -1,6 +1,109 @@
-(function() {
-  console.log('hello there!');
+// Setup Variables
+var resolutions = []
+var socket = io();
+var count = 0
+//Hide Cards
+$(".card-container").hide();
+$("header").hide();
 
-}).call(this);
+// Check For Cookies
+if (Cookies.get('added') == "true") {
+	$(".title").hide()
+	$(".card-container").show();
+	$("header").show();
+}
 
- //# sourceMappingURL=main.js.map
+$(document).keyup(function(e) {
+    if (e.keyCode == 27) { // escape key maps to keycode `27`
+	     
+
+
+    		var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    	$(".title").addClass('animated ' + "zoomOut").one(animationEnd, function() {
+        	$(this).removeClass('animated ' + "zoomOut");
+        	$(this).hide()
+        	console.log("Done")
+            $(".card-container").show()
+            $("header").show()
+
+	   	 });
+	}
+});
+
+//Receive Data
+socket.on('data', function (data) {
+ 	resolutions.push(data)
+ 	console.log(resolutions)
+ 	if (count > 80){
+ 		;
+		var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    	$(".card-container").find('div:first').addClass('animated ' + "bounceOutLeft").one(animationEnd, function() {
+        	$(this).removeClass('animated ' + "bounceOutLeft");
+        	$(".card-container").find('div:first').remove()
+	   	 });
+ 	}
+ 	$(".card-container").append("<div class='card'><p>" + data + " </p></div>")
+ 	count ++;
+
+	});
+
+
+
+
+$(".add").click(function(e) {
+
+		//Animate Title
+		var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    
+
+
+    		$("header").addClass('animated ' + "fadeOutUp").one(animationEnd, function() {
+    			$(this).removeClass('animated ' + "fadeOutUp");
+        		$(this).hide()
+        		});
+			$(".card-container").hide();
+   			$(".title").show();
+			$(".title").addClass('animated ' + "fadeInDown")
+	   	 
+	e.preventDefault();
+
+});
+
+
+//Form Handling
+$("form").submit(function(e) {
+	//Disallow empty inputs
+    if ((($(".input").val()).trim()) == ""){
+    	alert("Please enter a value")
+    } 
+   	else {
+		$.ajax({
+	       type: "POST",
+	       url: "/",
+	       data: $(".input").serialize(), // serializes the form's elements.
+	       success: function(data){}
+	    });
+
+		//Animate Title
+		var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    	$(".title").addClass('animated ' + "zoomOut").one(animationEnd, function() {
+        	$(this).removeClass('animated ' + "zoomOut");
+        	$(this).hide()
+        	console.log("Done")
+            Cookies.set('added', true)
+            $(".card-container").show()
+            $("header").show()
+
+	   	 });
+
+	}
+	e.preventDefault();
+});
+
+
+// Maintenance Function
+var removecookies = function(){
+	Cookies.remove('added')
+}
+
+//(this).find('span:first').remove();
--- Server/server.js
+++ Server/server.js
@@ -8,35 +8,67 @@ var io = require('socket.io').listen(server);
 var MongoClient = require('mongodb').MongoClient;
 var format = require('util').format;
 app.use(bodyParser());
-
+var url = 'mongodb://localhost:27017/Resolutions';
+var swearjar = require('swearjar');
 
 //Listen on port 8080
 server.listen(port)
 console.log("Server Started On Port " + port)
-resolutions = []
-
-var url = 'mongodb://localhost:27017/Resolutions';
-MongoClient.connect(url, function (err, db){
-if (err)throw err;
-console.log("Connected correctly to server");
 
 
-db.collection('data').find({}).toArray(function(err, doc){
-    resolutions = [];
-	for (x = 0; x < doc.length; x++){
-	  	resolutions.push(doc[x].resolution);
-	} 
-	console.log(resolutions)
-  });	
 
+ var readnew = function(socket) {
+	MongoClient.connect(url, function (err, db){
+	if (err)throw err;
+	console.log("Connected correctly to server");
+
+	db.collection('data').find({}).toArray(function(err, doc){
+		for (x = 0; x < doc.length; x++){
+		  	socket.emit('data', doc[x].resolution)
+		  	console.log(doc[x].resolution)
+		} 
+		db.close()
+	  });	
+	
 });
 
+ }
+
+
+var addnew = function(resolution){
+	MongoClient.connect(url, function (err, db){
+		if (err)throw err;
+		console.log("Connected correctly to server");
+		db.collection("data").insert({"resolution" : resolution}, function(err, doc) {
+			    if(err){
+			    	throw err;
+			    } else { 
+			    	console.log("Item Added") 
+			    }
+			    db.close()
+		});
+		
+	});
+}
 
 //Define POST route for /
 app.post('/', function(req, res){
 	console.log(1, "Request for /")
 	
+	if (swearjar.profane(req.body.resolution) == false) {
+		io.emit('data', req.body.resolution)
+		addnew(req.body.resolution)
+	} else {
+		console.log("Profanity Detected")
+	}
+	
 });
 
+io.sockets.on('connection', function (socket) {
+    console.log("New Connection")
+    readnew(socket)
+});
 //Define GET route for /
 app.use('/', express.static(__dirname + "/public/"))
+
+// readnew()
